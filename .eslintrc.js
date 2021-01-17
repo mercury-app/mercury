8ca798a407b147ae2ad4cfe1af0ea742feff9385
Saiful B. Khan <saifulbkhan@gmail.com>
2021-01-17 19:07:11 +0530

    Add toy canvas

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+/* eslint-disable quote-props */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    'semi': [1, 'always'],
+    'space-before-function-paren': ['error', {
+      'anonymous': 'always',
+      'named': 'never',
+      'asyncArrow': 'always'
+    }]
+  }
+};
